2ab3482d4e195508693cfb2cd12dbae372e373d5
Tomer Groisman <tomergroisman@gmail.com>
2021-04-18 14:11:22 +0000

    Update api routes response

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from classes.register import Register
 from database.database import Database as db
 
@@ -7,14 +7,13 @@ api = Blueprint('api', __name__)
 
 # Register new school route
 @api.route('/api/school', methods=['GET', 'POST'])
-def add_school():
+def school_route():
     if request.method == 'GET':
         db.connect()
         docs = db.get('Schools', {})
         db.disconnect()
-        print(json.loads(docs[0].to_json()))
-        return json.loads(docs[0].to_json())
-
+        return jsonify([doc.to_json() for doc in docs])
+        
     if request.method == 'POST':
         _school = request.get_json()
         school_doc = Register.add("Schools", _school)
@@ -22,17 +21,31 @@ def add_school():
         return school_doc
 
 # Register new class route
-@api.route('/api/class', methods=['POST'])
-def add_class():
-    _class = request.get_json()
-    class_doc = Register.add("Classes", _class)
-    print("REGISTERED new class: " + class_doc.id)
-    return class_doc
+@api.route('/api/class', methods=['GET', 'POST'])
+def class_route():
+    if request.method == 'GET':
+        db.connect()
+        docs = db.get('Classes', {})
+        db.disconnect()
+        return jsonify([doc.to_json() for doc in docs])
+
+    if request.method == 'POST':
+        _class = request.get_json()
+        class_doc = Register.add("Classes", _class)
+        print("REGISTERED new class: " + class_doc.id)
+        return class_doc
 
 # Register new student route
-@api.route('/api/student', methods=['POST'])
-def add_student():
-    _student = request.get_json()
-    student_doc = Register.add("Students", _student)
-    print("REGISTERED new student: " + student_doc.id)
-    return student_doc
+@api.route('/api/student', methods=['GET', 'POST'])
+def student_route():
+    if request.method == 'GET':
+        db.connect()
+        docs = db.get('Students', {})
+        db.disconnect()
+        return jsonify([doc.to_json() for doc in docs])
+
+    if request.method == 'POST':
+        _student = request.get_json()
+        student_doc = Register.add("Students", _student)
+        print("REGISTERED new student: " + student_doc.id)
+        return student_doc
