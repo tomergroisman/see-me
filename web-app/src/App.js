5d11813a722a07fd3cc066c824db47dbfe1f8fde
Gilad <gilade@bridgify.io>
2021-04-22 09:40:41 +0300

    create react app + MVP UI interface

---
+++ web-app/src/App.js
@@ -0,0 +1,73 @@
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import { Button } from "@material-ui/core";
+function App() {
+  const [reportValue, setReportValue] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
+
+  const emojisDic = {
+    "-2": "😥",
+    "-1": "😔",
+    0: "😐",
+    1: "😊",
+    2: "😄",
+  };
+
+  async function handleSubmit() {
+    setSubmitted(true);
+    await axios.post(process.env.REACT_APP_API_URL, { report: reportValue });
+  }
+
+  const ThankYouMessage = () => (
+    <>
+      <h1> תודה רבה על הדיווח! </h1>
+      <div style={{ fontSize: "64px" }}>🙏</div>{" "}
+    </>
+  );
+
+  const ReportForm = () => (
+    <>
+      <h1 style={{ marginBottom: "32px" }}> מה ברצונך לדווח?</h1>
+
+      <div className="slidecontainer">
+        <input
+          dir="ltr"
+          type="range"
+          min="-2"
+          max="2"
+          value={reportValue}
+          className="slider"
+          onInput={(e) => setReportValue(e.target.value)}
+        />
+      </div>
+
+      <div style={{ fontSize: "64px", marginBottom: "64px" }}>
+        {emojisDic[reportValue]}
+      </div>
+
+      <Button
+        disabled={reportValue === 0}
+        variant="contained"
+        color="primary"
+        style={{ fontSize: "20px" }}
+        onClick={handleSubmit}
+      >
+        שליחה
+      </Button>
+    </>
+  );
+
+  const RenderedComponent = submitted ? ThankYouMessage : ReportForm;
+  return (
+    <div
+      className="App"
+      dir="rtl"
+      style={{ padding: "5%", paddingTop: "52px" }}
+    >
+      <RenderedComponent />
+    </div>
+  );
+}
+
+export default App;
