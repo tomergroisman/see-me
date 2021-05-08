e443fc3ab1a4fad45bbf903ae6a20d59e65ec46e
Tomer Groisman <tomergroisman@gmail.com>
2021-05-08 10:02:48 +0000

    Change entry point name

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -e
 
 source bin/activate
-python src/web_server.py
-# flask run --host=0.0.0.0 --port=3000
-# trap 'kill %1' SIGINT
-# flask run --host=0.0.0.0 --port=3000 & python src/mqtt_client.py
+python src/server.py
