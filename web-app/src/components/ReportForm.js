01b5479402e3405b23dcbe24526cc689cc9cca7b
giladefrati <egilad7@gmail.com>
2021-06-15 15:51:00 +0300

    <UI> Button height fixes

--- web-app/src/components/ReportForm.js
+++ web-app/src/components/ReportForm.js
@@ -25,63 +25,57 @@ export default function ReportForm({
         reportValue={reportValue}
         setReportValue={setReportValue}
       />
-      {reportValue !== 0 ? <>
-        {/* <div style={{ textAlign: 'right' }}>
+      <div style={{ minHeight: '15vh', width: '100%' }}>
+        {reportValue !== 0 ?
+          <StyledTextField
+            scale={reportValue !== 0 ? 1 : 0}
+            label=""
+            value={message}
+            helperText="*  השיתוף  הינו אנונימי"
+            onChange={(e) => handleTyping(e)}
+            margin="normal"
+            variant="outlined"
+            // placeholder="ניתן גם להוסיף כמה מילים.."
+            placeholder="מה הייתה החוויה? איזה רגש עלה בך? איך בחרת להתמודד?"
+            multiline
+            rows={3}
+          // className="expandable"
+          // FormHelperTextProps={{marginLeft:'0'}}
+          /> : null}
+      </div>
 
-          <AddSomeWordsLabel>
-            ניתן גם להוסיף כמה מילים..
-        </AddSomeWordsLabel>
-        </div> */}
-        <StyledTextField
-          scale={reportValue !== 0 ? 1 : 0}
-          label=""
-          value={message}
-          helperText="*  השיתוף  הינו אנונימי"
-          onChange={(e) => handleTyping(e)}
-          margin="normal"
-          variant="outlined"
-          // placeholder="ניתן גם להוסיף כמה מילים.."
-          placeholder="מה הייתה החוויה? איזה רגש עלה בך? איך בחרת להתמודד?"
-          multiline
-          rows={3}
-        // className="expandable"
-        // FormHelperTextProps={{marginLeft:'0'}}
-        /> </> : null}
-      {/* <div style={{ position: 'absolute', bottom: '2vh', width: '100%' }}> */}
 
-        <StyledButton
-          disabled={reportValue == 0}
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-        >
-          שיתוף
+      <StyledButton
+        disabled={reportValue == 0}
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit}
+      >          שיתוף
       </StyledButton>
-      {/* </div> */}
     </>
   );
 }
 
 const StyledButton = styled(Button)`
-  font-size: 2vh;
-  border-radius: 15px;
-  width: 70%;
-  height: 46px;
-  margin-top: 5vh;
-  position:relative;
-`;
+      font-size: 2vh;
+      border-radius: 15px;
+      width: 70%;
+      height: 46px;
+      margin-top: 5vh;
+      position:relative;
+      `;
 const StyledTextField = styled(TextField)`
-  width:90%;
-  overflow: hidden;
+      width:90%;
+      overflow: hidden;
 
-  transition: all 2.5s ease-in-out;
-  transform-origin: left top;
-  transform: scaleY(${props => props.scale});
-`;
+      transition: all 2.5s ease-in-out;
+      transform-origin: left top;
+      transform: scaleY(${props => props.scale});
+      `;
 
 const AddSomeWordsLabel = styled.div`
-  text-align:right;
-  color:#444444;
-  width:90%;
-  margin-top:3vh;
-`;
+      text-align:right;
+      color:#444444;
+      width:90%;
+      margin-top:3vh;
+      `;
